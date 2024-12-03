2643122a0551a377e843e0956a6bf968decf8054
Colin Houghton <choughton@justworks.com>
2024-12-02 21:17:49 -0500

    feat: Add palette conversion function for theme colors

---
+++ lua/wezterm-theme-loader/palette.lua
@@ -0,0 +1,40 @@
+M = {}
+
+-- Example from a theme file:
+--
+--
+--  {
+--    "colors": {
+--      "ansi": [
+--        "#15161e",
+--        "#f7768e",
+--        "#9ece6a",
+--        "#e0af68",
+--        "#7aa2f7",
+--        "#bb9af7",
+--        "#7dcfff",
+--        "#a9b1d6"
+--      ],
+--      "background": "#1a1b26",
+--      "brights": [
+--        "#414868",
+--        "#f7768e",
+--        "#9ece6a",
+--        "#e0af68",
+--        "#7aa2f7",
+--        "#bb9af7",
+--        "#7dcfff",
+--        "#c0caf5"
+--      ],
+--      "cursor_bg": "#c0caf5",
+--      "cursor_border": "#c0caf5",
+--      "cursor_fg": "#1a1b26",
+--      "foreground": "#c0caf5",
+--      "selection_bg": "#283457",
+--      "selection_fg": "#c0caf5",
+--  },
+
+M.convert_ansi_to_palette = function(theme_colors)
+  palette_colors = {}
+  return palette_colors
+end
