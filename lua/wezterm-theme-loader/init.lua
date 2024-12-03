17ed1d588d1548a67f1edb8707570549e8f5289c
Colin Houghton <choughton@justworks.com>
2024-12-02 20:51:23 -0500

    feat: Add auto theme download with improved setup logic

--- lua/wezterm-theme-loader/init.lua
+++ lua/wezterm-theme-loader/init.lua
@@ -3,10 +3,12 @@ local M = {}
 ---Setup the wezterm-theme-loader plugin
 ---@param opts UserOptions: plugin options
 M.setup = function(opts)
+  -- Initial housekeeping stuff
   require("wezterm-theme-loader.config").setup(opts)
   require("wezterm-theme-loader.cmd").create_cmds()
 
-  if M.options.auto_fetch_on_launch and not require("wezterm-theme-loader.fetch").check_output_path() then
+  -- Auto fetch the file
+  if not require("wezterm-theme-loader.fetch").check_output_path() then
     require("wezterm-theme-loader.fetch").download_themes()
   end
 end
